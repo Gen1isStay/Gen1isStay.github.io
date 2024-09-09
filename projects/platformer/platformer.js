a451c08c52a01c73681d588ada22fea5768934d7
Gen1isStay <genesis.chaconcarabantes@livingstoncollegiate.org>
2024-09-09 14:42:24 +0000

    <message>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -39,7 +39,11 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
-
+    createPlatform(1000, 650, 200, 20); //short but wide platform located 1000 pixels from the left of the screen and 650 pixels from the top of the screen
+    createPlatform(350,200,200,20); //short but wide platform located 350 pixels from the right of the screen and 200 pixels from the bottom of the screen
+    createPlatform(500, 300, 200, 20); //short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    createPlatform(1100, 550, 200, 20); //short but wide platform located 1000 pixels from the left of the screen and 700 pixels from the top of the screen
+    createPlatform(800, 435, 200, 20); //short but wide platform located 1000 pixels from the left of the screen and 700 pixels from the top of the screen
 
     
     // TODO 2
@@ -48,15 +52,17 @@ $(function () {
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
 
-
+    createCollectable("steve", 200, 170, 6, 0.7);
+    createCollectable("steve", 400, 100, 6, 0.7);
+    createCollectable("steve", 800, 300, 6, 0.7);
 
     
     // TODO 3
-    // Create cannons
+    // Create cannons"
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+    createCannon("right",400,500);
 
 
     
